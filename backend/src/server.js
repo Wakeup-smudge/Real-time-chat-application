5064563f547d3cc6ea22634d89fa89d1caeba582
Wakeup-smudge <sumitpant4986@gmail.com>
2025-11-28 13:20:58 +0545

    fixing errors

--- backend/src/server.js
+++ backend/src/server.js
@@ -17,7 +17,7 @@ app.use("/api/messages", messageRoutes);
 
 // make ready for deployment
 if(process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "../frontend/vite-project/dist")))
+    app.use(express.static(path.join(__dirname, "../../frontend/vite-project/dist")))
 }
 
 app.get("*", (req,res) => {
