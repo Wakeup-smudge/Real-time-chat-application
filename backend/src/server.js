20b1dfa2bfc669f65e3163336aef5fe3c36bb9ca
Wakeup-smudge <sumitpant4986@gmail.com>
2025-11-28 13:15:50 +0545

    fixing errors

--- backend/src/server.js
+++ backend/src/server.js
@@ -21,7 +21,7 @@ if(process.env.NODE_ENV === "production") {
 }
 
 app.get("*", (req,res) => {
-    res.sendFile(path.join(__dirname,"../frontend/vite-project/dist/index.html"))
+    res.sendFile(path.join(__dirname,"../../frontend/vite-project/dist/index.html"))
 })
 
 app.listen(PORT, () => 
